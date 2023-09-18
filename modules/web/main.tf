35702b97d365b32cb6b53511f5cec5d06ea15a12
Raza Mehdi <srmk@outlook.com>
2023-09-18 16:47:10 +0500

    Add security group using TF.

--- modules/web/main.tf
+++ modules/web/main.tf
@@ -1,12 +1,54 @@
+resource "aws_security_group" "sg-test-web" {
+  name = "sg_test_web"
+  description = "Security Group for Web Server"
+  vpc_id = "vpc-9bfd31fe"
+
+  ingress {
+    from_port = "80"
+    to_port = "80"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web_server" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name = "TF-TEST"
+  vpc_security_group_ids = ["${aws_security_group.sg-test-web.id}"]
 
   tags = {
     Name = var.instance_tag
   }
 
-  user_data = file("${path.module}/lemp_ubuntu.sh")
+  provisioner "file" {
+    source      = "${path.module}/default-host.conf"
+    destination = "/home/ubuntu/default-host.conf"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/www.conf"
+    destination = "/home/ubuntu/www.conf"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/lemp_ubuntu.sh"
+    destination = "/home/ubuntu/lemp_ubuntu.sh"
+  }
 
   root_block_device {
     delete_on_termination = "true"
