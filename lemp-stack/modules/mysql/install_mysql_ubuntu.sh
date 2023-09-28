a04332c6d2f202a333816df9a108ea7ac7e64911
Raza Mehdi <srmk@outlook.com>
2023-09-29 01:21:42 +0500

    Fix mysql script execution for TF.

--- lemp-stack/modules/mysql/install_mysql_ubuntu.sh
+++ lemp-stack/modules/mysql/install_mysql_ubuntu.sh
@@ -10,6 +10,6 @@ sudo DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt-get install -y mys
 
 password=$(cat /home/ubuntu/mysql_root_password)
 
-printf "The MySQL root password is: %s\n" "$password"
+echo "The MySQL root password is: $password"
 
 echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$password';" | sudo mysql
